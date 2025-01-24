cc717e559f0ab3fe825ba8f68dfda42a5a485dab
HelgaZhizhka <helgazhyzhka@gmail.com>
2025-01-24 19:27:41 +0100

    merge from master

--- src/js/renderCountryList.js
+++
@@ -1,9 +0,0 @@
-const renderCountryList = (result) => {
-  const dataList = document.querySelector('#country-suggestions');
-  //TODO aoi request to get the list of countries
-  // const countries = await fetchCountries();
-
-  // dataList.innerHTML = countries.map(country => `<option value="${country}"></option>`).join('');
-};
-
-export default renderCountryList;
