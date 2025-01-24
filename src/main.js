6a59b0771a8a693450c2901a26fea81dee9f110a
Misha <mishazhizhka@gmail.com>
2025-01-24 22:27:01 +0200

    developing logic for loading more content

--- src/main.js
+++ src/main.js
@@ -17,10 +17,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (!form) return;
 
+  const divider = document.getElementById("divider");
+
+  let page = 1;
+  const loadMoreResults = () => {
+    page += 1;
+    if (form.name === 'searchForm') {
+      handleFormSubmit(form, fetchSearchForm, renderResults, page);
+    } else if (form.name === 'searchTopHeadlinesForm') {
+      handleFormSubmit(form, fetchSearchHeadlinesForm, renderResults);
+    }
+  }
+
+  const handleIntersection = entries => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+         //console.log(entry.isIntersecting);
+         loadMoreResults();
+         console.log(page);
+      }
+    })
+  }
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  }
+  const observer = new IntersectionObserver(handleIntersection, options);
+  observer.observe(divider);
+
   if (form.name === 'searchForm') {
-    handleFormSubmit(form, fetchSearchForm, renderResults);
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      handleFormSubmit(form, fetchSearchForm, renderResults);
+    });
   } else if (form.name === 'searchTopHeadlinesForm') {
-    handleFormSubmit(form, fetchSearchHeadlinesForm, renderResults);
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      handleFormSubmit(form, fetchSearchHeadlinesForm, renderResults);
+    });
   }
 
   if (radioButtons.length === 0) return;
