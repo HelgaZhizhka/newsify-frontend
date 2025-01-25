d7a58854a3217a56cc9e27b64e2e99d11b559812
Misha <mishazhizhka@gmail.com>
2025-01-25 17:46:36 +0200

    working on load more

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import './scss/app.scss'
 import themeToggle from './js/theme.js'
 import handleFormSubmit from './js/handleFormSubmit.js'
-import { fetchResults } from './js/api.js'
+import { fetchNews } from './js/api.js'
 import renderResults from './js/renderResults.js'
+import showLoader from './js/showLoader.js'
+import hideLoader from './js/hideLoader.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('search-form')
@@ -10,30 +12,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
   themeToggle('#themeToggle')
 
-  const showLoader = () => {
-    const resultsContainer = document.getElementById('results')
-    resultsContainer.innerHTML =
-      '<div class="loader"><svg class="icon" width="52" height="52" xmlns="http://www.w3.org/2000/svg"><use href="#icon-loader"></use></svg></div>'
+  if (!form) return
+
+  let currentPage = 1;
+  let loading = false;
+
+  const handleSearchForm = async () => {
+    try {
+      showLoader();
+      await handleFormSubmit(form, fetchNews, renderResults, currentPage);
+      hideLoader();
+    } catch (error) {
+      console.error(error);
+      hideLoader();
+    }
   }
 
-  if (!form) return
+  form.addEventListener('submit', async event => {
+    event.preventDefault();
+    await handleSearchForm();
+  })
+
+  if (radioButtons.length === 0) return
+
+  radioButtons.forEach(radio => {
+    radio.addEventListener('change', async () => {
+      if (radio.checked) {
+        await handleSearchForm();
+      }
+    })
+  })
 
   const divider = document.getElementById('divider')
 
-  let currentPage = 1
-  let loading = false;
-
   const handleIntersection = async entries => {
     if (entries[0].isIntersecting && !loading) {
-        loading = true;
-        currentPage++;
-
-      try {
-        await handleFormSubmit(form, fetchResults, renderResults, currentPage)
-      } catch (error) {
-        console.error(error)
-      }
-
+      loading = true;
+      currentPage += 1;
+      await handleSearchForm();
       loading = false;
     }
   }
@@ -46,23 +62,4 @@ document.addEventListener('DOMContentLoaded', () => {
   const observer = new IntersectionObserver(handleIntersection, options)
   observer.observe(divider)
 
-  form.addEventListener('submit', async event => {
-    event.preventDefault()
-    try {
-      showLoader()
-      await handleFormSubmit(form, fetchResults, renderResults)
-    } catch (error) {
-      console.error(error)
-    }
-  })
-
-  if (radioButtons.length === 0) return
-
-  radioButtons.forEach(radio => {
-    radio.addEventListener('change', () => {
-      if (radio.checked) {
-        form.dispatchEvent(new Event('submit', { cancelable: true }))
-      }
-    })
-  })
 })
